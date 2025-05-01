8aa764d1dd215af5a11529d782276f765c96d006
root <root@OpenWrt.lan>
2025-05-01 22:43:23 +0800

    update user-agent

--- update.js
+++ update.js
@@ -44,7 +44,7 @@ async function codeUpdate() {
 async function scrapeAndSave() {
   const url = "http://zxgk.court.gov.cn/getSxcjData";
   const UA =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0";
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0";
   await axios
     .get(url, {
       headers: {
